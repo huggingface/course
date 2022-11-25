3862faf712ed37f62baaa10f437925c1657212af
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2022-11-25 12:56:01 +0100

    Add script

--- subtitles/README.md
+++ subtitles/README.md
@@ -2,6 +2,26 @@
 
 This folder contains all the subtitles for the course videos on YouTube.
 
-## How to translate
+## How to translate the subtitles
 
-Add instructions.
+To translate the subtitles, we'll use two nifty libraries that can (a) grab all the YouTube videos associated with the course playlist and (b) translate them on the fly.
+
+To get started, install the following:
+
+```bash
+python -m pip install youtube_transcript_api youtube-search-python pandas tqdm
+```
+
+Next, run the following script:
+
+```bash
+python utils/generate_subtitles.py --language LANG_CODE
+```
+
+where `LANG_CODE` is the same language ID used to denote the chosen language the `chapters` folder. If everything goes well, you should end up with a set of translated `.srt` files with timestamps in `subtitles/LANG_CODE` along with some metadata in `metadata.csv`.
+
+Some languages like Simplified Chinese have a different language code to the one used in the course. For these languages, you also need to specify the YouTube language code, e.g.:
+
+```bash
+python utils/generate_subtitles.py --language zh-CN --youtube_language_code zh-Hans
+```
---
+++ utils/generate_subtitles.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from tqdm.auto import tqdm
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import SRTFormatter
+from youtubesearchpython import Playlist
+from pathlib import Path
+import argparse
+
+def generate_subtitles(language, youtube_language_code=None):
+    metadata = []
+    formatter = SRTFormatter()
+    path = Path(f"subtitles/{language}")
+    path.mkdir(parents=True, exist_ok=True)
+    playlist_videos = Playlist.getVideos(
+        "https://youtube.com/playlist?list=PLo2EIpI_JMQvWfQndUesu0nPBAtZ9gP1o"
+    )
+
+    for idx, video in enumerate(playlist_videos["videos"]):
+        video_id = video["id"]
+        title = video["title"]
+        title_formatted = title.lower().replace(" ", "-").replace(":", "").replace("?", "")
+        id_str = f"{idx}".zfill(2)
+        srt_filename = f"subtitles/{language}/{id_str}_{title_formatted}.srt"
+
+        # Skip course events
+        if "Event Day" in title:
+            continue
+
+        # Get transcript
+        try:
+            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+            english_transcript = transcript_list.find_transcript(language_codes=["en", "en-US"])
+            languages = pd.DataFrame(english_transcript.translation_languages)["language_code"].tolist()
+            # Map mismatched language codes
+            if language not in languages:
+                if youtube_language_code is None:
+                    raise ValueError(f"youtube_language_code must be set for {language}")
+                language_code = youtube_language_code
+            else:
+                language_code = language
+            translated_transcript = english_transcript.translate(language_code)
+            translated_transcript = translated_transcript.fetch()
+            srt_formatted = formatter.format_transcript(translated_transcript)
+            with open(srt_filename, "w", encoding="utf-8") as f:
+                f.write(srt_formatted)
+        except:
+            print(f"Could not find transcript for {title} with ID {video_id} at {video['link']}.")
+            with open(srt_filename, "w", encoding="utf-8") as f:
+                f.write("No transcript found for this video!")
+
+        metadata.append({"id": video_id, "title": title, "link": video["link"], "srt_filename": srt_filename})
+
+    df = pd.DataFrame(metadata)
+    df.to_csv(f"subtitles/{language}/metadata.csv", index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--language", type=str, help="Language to generate subtitles for")
+    parser.add_argument("--youtube_language_code", type=str, help="YouTube language code")
+    args = parser.parse_args()
+    generate_subtitles(args.language, args.youtube_language_code)
+    print(f"All done! Subtitles stored at subtitles/{args.language}")
