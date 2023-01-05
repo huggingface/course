f71cf6c3b4cb235bc75a14416c6e8a57fc3d00a7
lewtun <lewis.c.tunstall@gmail.com>
2023-01-06 00:02:26 +1100

    Add captions for tasks videos (#464)
    * Add captions for tasks videos
    
    * Fix script

--- utils/generate_subtitles.py
+++ utils/generate_subtitles.py
@@ -1,29 +1,43 @@
 import pandas as pd
-from tqdm.auto import tqdm
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
 from youtubesearchpython import Playlist
 from pathlib import Path
 import argparse
-import sys
+
+COURSE_VIDEOS_PLAYLIST = "https://youtube.com/playlist?list=PLo2EIpI_JMQvWfQndUesu0nPBAtZ9gP1o"
+TASK_VIDEOS_PLAYLIST = "https://youtube.com/playlist?list=PLo2EIpI_JMQtyEr-sLJSy5_SnLCb4vtQf"
+# These videos are not part of the course, but are part of the task playlist
+TASK_VIDEOS_TO_SKIP = ["tjAIM7BOYhw", "WdAeKSOpxhw", "KWwzcmG98Ds", "TksaY_FDgnk", "leNG9fN9FQU", "dKE8SIt9C-w"]
 
 
-def generate_subtitles(language: str, youtube_language_code: str = None):
+def generate_subtitles(language: str, youtube_language_code: str = None, is_task_playlist: bool = False):
     metadata = []
     formatter = SRTFormatter()
     path = Path(f"subtitles/{language}")
     path.mkdir(parents=True, exist_ok=True)
-    playlist_videos = Playlist.getVideos("https://youtube.com/playlist?list=PLo2EIpI_JMQvWfQndUesu0nPBAtZ9gP1o")
+    if is_task_playlist:
+        playlist_videos = Playlist.getVideos(TASK_VIDEOS_PLAYLIST)
+    else:
+        playlist_videos = Playlist.getVideos(COURSE_VIDEOS_PLAYLIST)
 
     for idx, video in enumerate(playlist_videos["videos"]):
         video_id = video["id"]
         title = video["title"]
         title_formatted = title.lower().replace(" ", "-").replace(":", "").replace("?", "")
         id_str = f"{idx}".zfill(2)
-        srt_filename = f"subtitles/{language}/{id_str}_{title_formatted}.srt"
+
+        if is_task_playlist:
+            srt_filename = f"{path}/tasks_{id_str}_{title_formatted}.srt"
+        else:
+            srt_filename = f"{path}/{id_str}_{title_formatted}.srt"
 
         # Skip course events
         if "Event Day" in title:
+            continue
+
+        # Skip task videos that don't belong to the course
+        if video_id in TASK_VIDEOS_TO_SKIP:
             continue
 
         # Get transcript
@@ -51,10 +65,14 @@ def generate_subtitles(language: str, youtube_language_code: str = None):
                 f.write("No transcript found for this video!")
 
         metadata.append({"id": video_id, "title": title, "link": video["link"], "srt_filename": srt_filename})
-        break
 
     df = pd.DataFrame(metadata)
-    df.to_csv(f"subtitles/{language}/metadata.csv", index=False)
+
+    if is_task_playlist:
+        df.to_csv(f"{path}/metadata_tasks.csv", index=False)
+    else:
+        df.to_csv(f"{path}/metadata.csv", index=False)
+
 
 
 if __name__ == "__main__":
@@ -62,5 +80,6 @@ if __name__ == "__main__":
     parser.add_argument("--language", type=str, help="Language to generate subtitles for")
     parser.add_argument("--youtube_language_code", type=str, help="YouTube language code")
     args = parser.parse_args()
-    generate_subtitles(args.language, args.youtube_language_code)
+    generate_subtitles(args.language, args.youtube_language_code, is_task_playlist=False)
+    generate_subtitles(args.language, args.youtube_language_code, is_task_playlist=True)
     print(f"All done! Subtitles stored at subtitles/{args.language}")
